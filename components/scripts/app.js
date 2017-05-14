2c4976f0b9ecf5d16ba4c2543c53df220a025d7f
Pawel Maricnkowski <wafel34@gmail.com>
2017-05-14 11:45:03 +0100

    Postcode validation

--- components/scripts/app.js
+++ components/scripts/app.js
@@ -4,6 +4,8 @@ var require = require;
 window.$ = window.jQuery = require('jquery');
 var bootstrap = require('bootstrap-sass');
 var nameRegEx = /[\d_+.,!@#$%\^&*();\/|<>"'?=+:]/g,
+	postCodes = /^[A-Za-z0-9]{2,4}\s+[A-Za-z0-9]{2,4}$/,
+	postCodesForbid = /[_+.,!@#$%\^&*();\/|<>"'?=+:]/g,
 	i,
 	inputItem = document.querySelectorAll('.person-details input.child');
 
@@ -18,6 +20,21 @@ function checkNames(source) {
 		source.className = 'child danger';
 	}
 	
+}
+
+//4. Valitaion of Postcodes
+function checkPostCode(source) {
+	'use strict';
+	var srcValue = source.value;
+	if (srcValue.search(postCodesForbid) === -1) {
+		if (postCodes.test(srcValue) === true) {
+			source.className = 'child passed';
+		} else {
+			source.className = 'child danger';
+		}
+	} else {
+		source.className = 'child danger';
+	}
 }
 
 
@@ -41,7 +58,9 @@ function myHandler(e) {
 		case 'city':
 			checkNames(src);
 			break;
-				
+		case 'postcode':
+			checkPostCode(src);
+			break;
 		}
 	} else {
 		src.className = 'child';
