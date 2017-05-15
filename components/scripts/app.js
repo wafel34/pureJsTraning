8688d95d700643804f58d386e94f0fe7a2b0981e
Pawel Marcinkowski <pmarcinkowski1016@timeinc.com>
2017-05-15 14:02:15 +0100

    added required fileds, amended method of iteration from for loop to array.map on input.child fields

--- components/scripts/app.js
+++ components/scripts/app.js
@@ -9,8 +9,29 @@ var nameRegEx = /[\d_+.,!@#$%\^&*();\/|<>"'?=+:]/g,
 	i,
 	inputItem = document.querySelectorAll('.person-details input.child');
 
+//3. Checking the input type.
+function checkType(src) {
 
-//3. Valitaion of First name, last name and city.
+	//Assinging the name attribute to the variable
+	srcName = src.name;
+	if (src.value !== '') {
+		switch (srcName) {
+		case 'first_name':
+		case 'last_name':
+		case 'city':
+			checkNames(src);
+			break;
+		case 'postcode':
+			checkPostCode(src);
+			break;
+		}
+	} else {
+		src.className = 'child';
+	}
+}
+
+
+//4. Valitaion of First name, last name and city.
 function checkNames(source) {
 	'use strict';
 	var srcValue = source.value;
@@ -19,10 +40,9 @@ function checkNames(source) {
 	} else {
 		source.className = 'child danger';
 	}
-	
 }
 
-//4. Valitaion of Postcodes
+//5. Valitaion of Postcodes
 function checkPostCode(source) {
 	'use strict';
 	var srcValue = source.value;
@@ -45,29 +65,12 @@ function myHandler(e) {
 		srcName;
 	e = e || window.event;
 	src = e.target || e.srcElement;
-	/*if (src.nodeName.toLowerCase() !== 'input') {
-		return;
-	}*/
-	
-	srcName = src.name;
-	
-	if (src.value !== '') {
-		switch (srcName) {
-		case 'first_name':
-		case 'last_name':
-		case 'city':
-			checkNames(src);
-			break;
-		case 'postcode':
-			checkPostCode(src);
-			break;
-		}
-	} else {
-		src.className = 'child';
-	}
+
+	//Calling checkType function.
+	checkType(src);
 }
 
 // 1. Adding event listeners to input items in .person-details section
-for (i = 0; i < inputItem.length; i = i + 1) {
-	inputItem[i].addEventListener('blur', myHandler, false);
-}
+inputItem.forEach(function(item){
+	return item.addEventListener('blur', myHandler, false);
+});
